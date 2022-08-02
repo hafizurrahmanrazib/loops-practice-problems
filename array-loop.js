de0765e2f32ed3a79f32274bbe44a661d781116d
hafizurrahmanrazib <hafizur201611044@gmail.com>
2022-08-03 01:29:38 +0600

    upto array-loop.js done

---
+++ array-loop.js
@@ -0,0 +1,15 @@
+/* for (var i = 0; i < 10; i++) {
+    console.log(i);
+} */
+// Target: Display every element of an array
+/* var numbers = [45, 87, 89, 56, 32, 51, 25, 188, 41, 25, 98];
+for (var i = 0; i < numbers.length; i++) {
+    var number = numbers[i];
+    console.log(number);
+}  */
+// Target: Show All Items Of An Array
+var items = ['bottle', 'mouse', 'sunglass', 'pen', 'notebook'];
+for (var i = 0; i < items.length; i++) {
+    var item = items[i];
+    console.log(item);
+}
