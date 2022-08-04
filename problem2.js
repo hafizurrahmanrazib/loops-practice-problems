2b724b8d657bed7050507fe94c78ecf85284c1e2
hafizurrahmanrazib <hafizur201611044@gmail.com>
2022-08-05 02:09:05 +0600

    Practice Challange 01 with for loop

--- problem2.js
+++ problem2.js
@@ -9,6 +9,8 @@ for (i = 0; i < dailyWorks.length; i++) {
 } */
 
 
+
+
 // Practice Challange 01 with Break
 /* var dailyWorks = ["Unlock Module When It's Time To 8 O'clock", "Instant Watch Video And Then Practice", "Take Notes When Needed In The Middle Of Video Watch", "Practice Each Module When Each Module Finish", "If Not Understand Any Module", "Join Support Session"];
 for (i = 0; i < dailyWorks.length; i++) {
@@ -20,9 +22,28 @@ for (i = 0; i < dailyWorks.length; i++) {
 } */
 
 
-// Practice Challange 01 with array length times for loop
-var dailyWorks = ["Unlock Module When It's Time To 8 O'clock", "Instant Watch Video And Then Practice", "Take Notes When Needed In The Middle Of Video Watch", "Practice Each Module When Each Module Finish", "If Not Understand Any Module", "Join Support Session"];
+
+
+
+// Practice Challange 01 with 10 times for loop
+/* var dailyWorks = ["Unlock Module When It's Time To 8 O'clock", "Instant Watch Video And Then Practice", "Take Notes When Needed In The Middle Of Video Watch", "Practice Each Module When Each Module Finish", "If Not Understand Any Module", "Join Support Session"];
+for (i = 1; i <= 10; i++) {
+    console.log(dailyWorks);
+} */
+
+
+
+
+
+// Practice Challange 01 with for loop
+/* var dailyWorks = ["Unlock Module When It's Time To 8 O'clock", "Instant Watch Video And Then Practice", "Take Notes When Needed In The Middle Of Video Watch", "Practice Each Module When Each Module Finish", "If Not Understand Any Module", "Join Support Session"];
 for (i = 0; i < dailyWorks.length; i++) {
     var dailyWork = dailyWorks[i];
+    if (dailyWorks[i] == dailyWorks[4]) {
+        continue;
+    }
     console.log(dailyWork);
-}
+} */
+
+
+
